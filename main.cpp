64ba855eb887cb82c907587f5b83d0e6530eb3f3
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2024-05-31 12:45:11 -0700

    run now working

--- main.cpp
+++ main.cpp
@@ -11,11 +11,12 @@ int main() {
 
 //PROGRAM
     dog1.run(5, 800);// dog runs 3+ legs
+    dog1.setHappiness(true); //addBone function makes dog happy
     dog1.wag(13, 200);//0 - 1000
     dog1.setWeight(89);//weight of dog over 0 lbs
     dog1.bark(9);//barking, depends on weight +/- 100 lbs
     dog1.setNumberOfLegs(4); //0 - 4 at least 3 to run
-    dog1.setHappiness(true); //addBone function makes dog happy
+
     dog1.addBone(); //sets happiness to true **dynamically declares a new Bone**
 
     return 0;
